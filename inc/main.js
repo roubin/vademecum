438107ad5792adbf4ed7e2a08f2689397dc750ee
roubin <manu.roubin@free.fr>
2015-02-26 23:34:28 +0100

    submit values

--- inc/main.js
+++ inc/main.js
@@ -5,10 +5,7 @@ $(document).ready(function(){
     var sigNorm = math.norm([sigXX, sigYY, sigXY], 2);
     var sigXXNorm=sigXX/sigNorm;
     var sigYYNorm=sigYY/sigNorm;
-    var sigXYNorm=sigXY/sigNorm;
-
-    
-    var debugVar = math.norm([sigXXNorm, sigYYNorm, sigXYNorm], 2);
+    var sigXYNorm=sigXY/sigNorm;  
     $(".rangeUser").change(function(){
 	if($(this).attr("id") == "rangeUserSigXX") {
 	    sigXX=parseFloat($(this).val());
@@ -41,18 +38,50 @@ $(document).ready(function(){
 	$("#debugResult").append(sigmaXX[imin] + ' for ' + sigXXNorm + '<br/>');
 	$("#debugResult").append(sigmaYY[imin] + ' for ' + sigYYNorm + '<br/>');
 	$("#debugResult").append(sigmaXY[imin] + ' for ' + sigXYNorm + '<br/>');
-
 	$("#rangeUserSigXX").val(sigmaXX[imin]);
 	$("#rangeUserSigYY").val(sigmaYY[imin]);
 	$("#rangeUserSigXY").val(sigmaXY[imin]);    
-
-	$("#displaySigXX").text(sigmaXX[imin]);
-	$("#displaySigYY").text(sigmaYY[imin]);
-	$("#displaySigXY").text(sigmaXY[imin]);
-
+	$("#displaySigXX").val(sigmaXX[imin].toFixed(3));
+	$("#displaySigYY").val(sigmaYY[imin].toFixed(3));
+	$("#displaySigXY").val(sigmaXY[imin].toFixed(3));
 	$("#imgMicroCell").attr('src', './img/'+imin+'.png');
+    });
 
-    });    
+
+    
+    $( "#userSubmit" ).click(function( event ) {
+	event.preventDefault();
+	var sigXX = math.number($("#displaySigXX").val()); $("#rangeUserSigXX").val(sigXX);
+	var sigYY = math.number($("#displaySigYY").val()); $("#rangeUserSigYY").val(sigYY);
+	var sigXY = math.number($("#displaySigXY").val()); $("#rangeUserSigXY").val(sigXY);
+	var sigNorm = math.norm([sigXX, sigYY, sigXY], 2);
+	var sigXXNorm=sigXX/sigNorm;
+	var sigYYNorm=sigYY/sigNorm;
+	var sigXYNorm=sigXY/sigNorm;
+	
+	var dist=1000.0; imin=0;
+	for (i = 0; i < math.size(sigmaXX); i++)
+	{
+	    var dbXX=math.number(sigmaXX[i]);
+	    var dbYY=math.number(sigmaYY[i]);
+	    var dbXY=math.number(sigmaXY[i]);
+	    var dbNorm = math.norm([dbXX, dbYY, dbXY], 2);
+	    var dbXXNorm=dbXX/dbNorm;
+	    var dbYYNorm=dbYY/dbNorm;
+	    var dbXYNorm=dbXY/dbNorm;
+	    var distTmp=math.norm([ dbXXNorm-sigXXNorm, dbYYNorm-sigYYNorm, dbXYNorm-sigXYNorm], 2);
+	    if(dist>distTmp) {
+		dist=distTmp; imin=i;		
+	    }
+	}
+	$("#rangeUserSigXX").val(sigmaXX[imin]);
+	$("#rangeUserSigYY").val(sigmaYY[imin]);
+	$("#rangeUserSigXY").val(sigmaXY[imin]);    
+	$("#displaySigXX").val(sigmaXX[imin].toFixed(3));
+	$("#displaySigYY").val(sigmaYY[imin].toFixed(3));
+	$("#displaySigXY").val(sigmaXY[imin].toFixed(3));
+	$("#imgMicroCell").attr('src', './img/'+imin+'.png');
+    });
 });
 
 
